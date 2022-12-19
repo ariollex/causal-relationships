9cfef96c811d1f8eea677afeceea9562420d2ecf
Ariollex <ariollex@gmail.com>
2022-12-19 16:39:02 +0300

    continue

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas
 file_loc = 'Dataset/Cause-effect-pairs-in-school.csv'
 data = pandas.read_csv(file_loc)
 formatcauses = []
+info = []
 
 # Дебаг датасета
 # print(data.head)
@@ -13,6 +14,22 @@ for i in range(0, data.shape[0]): # пробег по всей таблице
         formatcauses.append([data.name[i], schoolclass, data.causes[i]]) # Присваивание массива с именем, классом и случаем в массив causes
 
 # Дебаг массива
+# for i in range(len(formatcauses)):
+#     print(formatcauses[i])
+# print(len(formatcauses))
+
 for i in range(len(formatcauses)):
-    print(formatcauses[i])
-print(len(formatcauses))
+    print(i+1, formatcauses[i])
+print("Выберите нужный случай и введите его номер: ", end='')
+userchoise = int(input()) - 1
+print()
+print('Вы выбрали номер ', userchoise, '. Случай связан с учеником по имени ', formatcauses[userchoise][0], sep='')
+print()
+
+for i in range(len(formatcauses)):
+    if formatcauses[userchoise][1] == formatcauses[i][1] and userchoise != i:
+        info.append(['Случай напрямую связан с', formatcauses[i][1], 'классом'])
+        break
+
+for i in range(len(info)):
+    print(*info[i])
