89b802d4c5062729523fef55ef0e92e3cbd5b6ea
Ariollex <ariollex@gmail.com>
2022-12-25 08:24:21 +0300

    Изменил тип данных 0, где он означает пустую строку

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy
 # Датасет
 file_loc = 'Dataset/Cause-effect-pairs-in-school.xlsx'
 data = pandas.read_excel(file_loc)
-data.replace(numpy.nan, '0', inplace=True)
+data.replace(numpy.nan, 0, inplace=True)
 
 # Настройки датасета
 name = data['Имя']
@@ -18,42 +18,46 @@ formatcauses = []
 info = []
 userchoise = -1
 
+
 def printinfo(info):
     print()
     for i in range(len(info)):
         print(*info[i])
+
 
 def debuglist(list):
     print()
     for i in range(len(list)):
         print(list[i])
 
+
 def debugdataset(dataset):
     print(dataset.head)
     print(dataset.shape)
+
 
 # Дебаг датасета
 # debugdataset(data)
 
 # Список инцидентов
 for i in range(0, data.shape[0]):
-    if causes[i] != '0':
+    if causes[i] != 0:
         schoolclass = str(parallel[i]) + letter[i]
         formatcauses.append([name[i], schoolclass])
 # Дебаг списка
 # debuglist(formatcauses)
 
 for i in range(len(formatcauses)):
-    print(i + 1,') ', sep='', end='')
+    print(i + 1, ') ', sep='', end='')
     print(*formatcauses[i])
 
 print("Выберите нужный случай и введите его номер: ", end='')
 
 while True:
     userchoise = input()
-    if userchoise.isdigit() == False:
+    if not userchoise.isdigit():
         print('Такого номера нет. Введите его заново: ')
-    elif not(0 < int(userchoise) < len(formatcauses) + 1):
+    elif not (0 < int(userchoise) < len(formatcauses) + 1):
         print('Такого номера нет. Введите его заново: ')
     else:
         userchoise = int(userchoise) - 1
