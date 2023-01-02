8e84d10ed8ef5a217e075ac0044f8cfafa8cacee
Ariollex <ariollex@gmail.com>
2023-01-02 12:02:39 +0300

    Упростил систему выбора пользователем варианта
    Убрал бессмысленный цикл и бессмысленное возвращение кодов

--- input_data.py
+++ input_data.py
@@ -8,12 +8,9 @@ def make_user_choice(example_list):
             exit()
         elif choice == 'L':
             change_language()
-            return -2
         elif not choice.isdigit():
             print(print_on_language(1, 1), end=': ')
         elif not (0 < int(choice) < len(example_list) + 1):
             print(print_on_language(1, 1), end=': ')
         else:
-            choice = int(choice) - 1
-            return choice
-        return -1
+            return int(choice) - 1
--- main.py
+++ main.py
@@ -41,24 +41,19 @@ graphs = [print_on_language(1, 5), print_on_language(1, 18)]
 
 # Program operation mode selection
 functions = [print_on_language(1, 8), print_on_language(1, 9)]
-while choice_mode == -2 or choice_mode == -1:
-    if choice_mode == -2:
-        print(print_on_language(1, 6) + ':')
-        for i in range(len(functions)):
-            print(i + 1, ') ', functions[i], sep='')
-        print(print_on_language(1, 7) + ':', end=' ')
-    choice_mode = input_data.make_user_choice(functions)
-
+print(print_on_language(1, 6) + ':')
+for i in range(len(functions)):
+    print(i + 1, ') ', functions[i], sep='')
+print(print_on_language(1, 7) + ':', end=' ')
+choice_mode = input_data.make_user_choice(functions)
 # Creating a list of incidents
 list_incidents = calculations.make_list_incidents(data, name, sex, parallel, letter, causes, info_about_causes,
                                                   time_causes)
 
 if choice_mode == 0:
+    print_data.print_list_incidents(list_incidents)
     # Incident selection
-    while user_selection == -2 or user_selection == -1:
-        if user_selection == -2:
-            print_data.print_list_incidents(list_incidents)
-        user_selection = input_data.make_user_choice(list_incidents)
+    user_selection = input_data.make_user_choice(list_incidents)
 
     print(print_on_language(1, 2), ' ', user_selection + 1,
           '. ' + print_on_language(2, 2) + ': '
@@ -78,8 +73,7 @@ elif choice_mode == 1:
         print(i + 1, ') ', graphs[i], sep='')
     print(print_on_language(1, 11) + ':', end=' ')
     # Graph selection
-    while choice_graph == -2 or choice_graph == -1:
-        choice_graph = input_data.make_user_choice(graphs)
+    choice_graph = input_data.make_user_choice(graphs)
     if choice_graph == 0:
         charts.graph_1(data, causes, list_incidents)
     elif choice_graph == 1:
