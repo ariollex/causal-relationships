e825abb524250419eb14f0d64ce58bf709c0b75a
Ariollex <ariollex@gmail.com>
2023-01-22 11:17:26 +0300

    Удалил функцию print_info
    Не используется

--- print_data.py
+++ print_data.py
@@ -1,12 +1,6 @@
 from strings import print_on_language
 
 
-def print_info(info):
-    print()
-    for i in range(len(info)):
-        print(*info[i])
-
-
 def print_list_incidents(example_list_incidents):
     for i in range(len(example_list_incidents)):
         print(i + 1, ') ', sep='', end='')
@@ -32,7 +26,7 @@ def is_fight(in_class, participants, student_class, suspicious, maximum, student
             result = result + '\n' + print_on_language(1, 24) + ' ' + maximum[0] + ' ' + \
                      ', ' + print_on_language(2, 24) + ' ' + str(maximum[1]) + ' ' + print_on_language(3, 24)
     result = result + '\n' + print_on_language(1, 25) + ' ' + str(len(participants) + 1) + ' ' + \
-             print_on_language(2, 25) + ':' + '\n' + student_name + '\n' + '\n'.join(participants)
+        print_on_language(2, 25) + ':' + '\n' + student_name + '\n' + '\n'.join(participants)
     return result
 
 
@@ -41,9 +35,9 @@ def is_incident_in_classroom(participants, student_class, suspicious, maximum, s
     result = result + print_on_language(1, 26).lower() + ' ' + print_on_language(2, 23) + ' ' + student_class
     if suspicious != 0:
         result = result + '\n' + print_on_language(1, 24) + ' ' + maximum[0] + ' ' + \
-                 print_on_language(2, 24) + ' ' + str(maximum[1]) + ' ' + print_on_language(3, 24)
+            print_on_language(2, 24) + ' ' + str(maximum[1]) + ' ' + print_on_language(3, 24)
     result = result + '\n' + print_on_language(1, 27) + ' ' + str(len(participants) + 1) + ' ' + \
-             print_on_language(2, 27) + ':' + '\n' + student_name + '\n' + '\n'.join(participants)
+        print_on_language(2, 27) + ':' + '\n' + student_name + '\n' + '\n'.join(participants)
     return result
 
 
