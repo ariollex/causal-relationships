c00b6c85cfb5326f75b7db9fa449ba141790d02e
Ariollex <ariollex@gmail.com>
2023-01-23 14:59:11 +0300

    Исправил неверное определение индекса для замены строки в configuration
    Это исправляет смену языка

--- strings.py
+++ strings.py
@@ -18,7 +18,7 @@ def set_language(language):
     if language != configuration[indexes[2]][
                    str(configuration[indexes[2]]).find("'") + 1:str(configuration[indexes[2]]).rfind("'")]:
         lines = open("configuration", 'r').readlines()
-        lines[2] = "language = '" + language + "'"
+        lines[indexes[2]] = "language = '" + language + "'"
         out = open("configuration", 'w')
         out.writelines(lines)
         out.close()
